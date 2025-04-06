4122024d79645c9f7d24a9483a49842bba0754c5
Aleks Gomeniuk <agsfinks@gmail.com>
2025-04-06 01:02:44 +0100

    refactor: pagination component to new base-component

--- src/components/pagination/index.ts
+++ src/components/pagination/index.ts
@@ -1,4 +1,11 @@
-import { directions } from '../../pages/garage/types';
+import { deleteCar } from '../../api/deleteCar';
+import { endpoints } from '../../api/endpoints';
+import { getData } from '../../api/getData';
+import { BaseElement } from '../../base/base-element';
+import { directions, ICar } from '../../pages/garage/types';
+import { buildUrl } from '../../utils/buildUrl';
+import { Button } from '../button/button';
+import styles from './pagination.module.css';
 
 const data = {
   back: '<<',
@@ -6,6 +13,10 @@ const data = {
   amount: '1/1',
   clear: 'delete list',
   ONE: 1,
+};
+
+const classes = {
+  container: styles['container'],
 };
 
 interface IParameters {
@@ -19,43 +30,59 @@ interface IConstructorParameters {
 }
 
 export class Pagination {
-  private _container: HTMLDivElement;
-  private _back: HTMLButtonElement;
-  private _next: HTMLButtonElement;
+  private _container: BaseElement<HTMLDivElement>;
+  private _back: Button;
+  private _next: Button;
   private _amount: HTMLDivElement;
+  private _clear: Button;
   private _changePage: (direction: directions) => void;
   private _clearList: () => void;
   constructor(parameters: IConstructorParameters) {
-    this._container = document.createElement('div');
-    this._back = document.createElement('button');
-    this._next = document.createElement('button');
-    this._amount = document.createElement('div');
+    this._container = new BaseElement<HTMLDivElement>({
+      tag: 'div',
+      className: classes.container,
+    });
+    this._back = new Button({
+      textContent: data.back,
+      callback: () => this.changePageHandler(directions.back),
+    });
+    this._next = new Button({
+      textContent: data.next,
+      callback: () => this.changePageHandler(directions.next),
+    });
+    this._amount = new BaseElement<HTMLDivElement>({
+      tag: 'div',
+      textContent: data.amount,
+    }).element;
+    this._clear = new Button({
+      textContent: data.clear,
+      callback: () => this.deletePageHandler(),
+    });
     this._changePage = parameters.changePage;
     this._clearList = parameters.clearList;
-    this.init();
+
+    this._container.append(
+      this._back.element,
+      this._amount,
+      this._next.element,
+      this._clear.element
+    );
   }
 
-  private init() {
-    this._back.textContent = data.back;
-    this._next.textContent = data.next;
-    this._amount.textContent = data.amount;
-    const clear = document.createElement('button');
-    clear.textContent = data.clear;
+  private async deletePageHandler() {
+    this._clear.element.disabled = true;
 
-    clear.addEventListener('click', async () => {
-      clear.disabled = true;
-      this._clearList();
-      clear.disabled = false;
+    const url = buildUrl(endpoints.BASE, endpoints.GARAGE, {});
+    const responseData = await getData<ICar[]>(url);
+    if ('error' in responseData) {
+      return;
+    }
+    const promises = responseData.data.map((car) => {
+      if (car.id) return deleteCar(car.id);
     });
-
-    this._back.addEventListener('click', () =>
-      this.changePageHandler(directions.back)
-    );
-    this._next.addEventListener('click', () =>
-      this.changePageHandler(directions.next)
-    );
-
-    this._container.append(this._back, this._amount, this._next, clear);
+    await Promise.all(promises);
+    this._clear.element.disabled = false;
+    this._clearList();
   }
 
   private changePageHandler(direction: directions) {
@@ -63,20 +90,20 @@ export class Pagination {
   }
 
   public update(parameters: IParameters) {
-    if (parameters.currentPage <= data.ONE) {
-      this._back.disabled = true;
-    } else {
-      this._back.disabled = false;
-    }
-    if (parameters.currentPage >= parameters.totalPages) {
-      this._next.disabled = true;
-    } else {
-      this._next.disabled = false;
-    }
+    if (parameters.currentPage <= data.ONE) this._back.element.disabled = true;
+    else this._back.element.disabled = false;
+
+    if (parameters.currentPage >= parameters.totalPages)
+      this._next.element.disabled = true;
+    else this._next.element.disabled = false;
+
     this._amount.textContent = `${parameters.currentPage} / ${parameters.totalPages}`;
+
+    if (!parameters.totalPages) this._clear.element.disabled = true;
+    else this._clear.element.disabled = false;
   }
 
-  public get htmlElement() {
-    return this._container;
+  public get element() {
+    return this._container.element;
   }
 }
