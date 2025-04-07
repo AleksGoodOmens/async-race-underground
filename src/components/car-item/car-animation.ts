73f649e5ed62de723fea4e3151ad8a1139348d5b
Aleks Gomeniuk <agsfinks@gmail.com>
2025-04-07 12:02:15 +0100

    feat: car animation and car item styles

---
+++ src/components/car-item/car-animation.ts
@@ -0,0 +1,28 @@
+export class CarAnimator {
+  private element: HTMLElement;
+
+  constructor(element: HTMLElement) {
+    this.element = element;
+  }
+
+  public start(distance: number, velocity: number, trackLength: number) {
+    const duration = distance / trackLength / velocity;
+
+    this.element.style.transition = `transform ${duration}s linear`;
+    this.element.style.transform = `translateX(${trackLength}px)`;
+  }
+
+  public stop() {
+    const computedStyle = getComputedStyle(this.element);
+    const matrix = new DOMMatrixReadOnly(computedStyle.transform);
+    const currentX = matrix.m41;
+
+    this.element.style.transition = 'none';
+    this.element.style.transform = `translateX(${currentX}px)`;
+  }
+
+  public reset() {
+    this.element.style.transition = 'none';
+    this.element.style.transform = 'translateX(0)';
+  }
+}
--- src/components/car-item/carItem.ts
+++ src/components/car-item/carItem.ts
@@ -1,132 +1,159 @@
+import { carEngineControl, IStartResponse } from '../../api/carEngineControl';
 import { deleteCar } from '../../api/deleteCar';
-import { updateCar } from '../../api/updateCar';
 import { BaseElement } from '../../base/base-element';
 import { ICar } from '../../pages/garage/types';
+import { shortString } from '../../utils/shortString';
 import { Button } from '../button/button';
 import { CarIcon } from './car';
+import { CarAnimator } from './car-animation';
 import styles from './carItem.module.css';
 
 interface IParameters {
-  type: 'manual' | 'auto';
   car: ICar;
   fetch: () => void;
+  tunning: (car: ICar) => void;
 }
 
 const data = {
-  placeholder: 'Car model',
-  delBtn: 'del',
-  updateBtn: 'update',
-  addBtn: 'add',
-  timeout: 20,
+  delBtn: 'delete',
+  select: 'select',
+  start: 'start',
+  stop: 'stop',
+  FULL_TRACK: 100,
+  START_INDEX: 0,
+  END_INDEX: -2,
 };
 
 const classes = {
   container: styles['container'],
+  track: styles['track'],
+  controls: styles['controls'],
+  car: styles['car'],
+  name: styles['name'],
+  delete: styles['delete'],
+  select: styles['select'],
+  start: styles['start'],
+  stop: styles['stop'],
 };
 
 export class CarItem {
   private _element: BaseElement<HTMLLIElement>;
-  private _name: BaseElement<HTMLInputElement>;
+  private _fetch: () => void;
+  private _tunning: (car: ICar) => void;
+  private _name: BaseElement<HTMLDivElement>;
+  private _deleteBtn: Button;
+  private _selectBtn: Button;
+  private _startBtn: Button;
+  private _stopBtn: Button;
+  private _track: BaseElement<HTMLDivElement>;
   private _model: CarIcon;
-  private _picker: BaseElement<HTMLInputElement>;
-  private _updateBtn: Button;
-  private _id: BaseElement<HTMLDivElement>;
+  private _carAnimator: CarAnimator;
+
   constructor(parameters: IParameters) {
-    this._element = new BaseElement<HTMLLIElement>({
+    this._fetch = parameters.fetch;
+    this._tunning = parameters.tunning;
+    this._element = new BaseElement({
       tag: 'li',
       className: classes.container,
     });
-    this._id = new BaseElement<HTMLDivElement>({
+    this._name = new BaseElement({
       tag: 'div',
-      textContent: `#${parameters.car.id}`,
+      className: classes.name,
+      textContent: shortString(parameters.car.name),
     });
-    this._name = new BaseElement<HTMLInputElement>({ tag: 'input' });
-    this._picker = new BaseElement<HTMLInputElement>({ tag: 'input' });
-    this._model = new CarIcon({ className: 'svg', color: '#fff' });
-    this._updateBtn = new Button({
-      textContent: data.updateBtn,
-      callback: () => {
-        this.handleUpdate(parameters.car.id);
-      },
-    });
-
-    if (parameters.type === 'auto')
-      this.viewAuto(parameters.car, () => parameters.fetch());
-  }
-
-  private viewAuto(car: ICar, fetch: () => void) {
-    this._name.element.placeholder = data.placeholder;
-    this._name.element.value = car.name;
-
-    this._model.color = car.color;
-
-    this._picker.element.type = 'color';
-    this._picker.element.value = car.color;
-
-    this._picker.element.addEventListener('input', () => {
-      const callback = this.handleChangeColor(this._picker.element.value);
-      callback();
-    });
-
-    const delButton = new Button({
+    this._deleteBtn = new Button({
       textContent: data.delBtn,
+      className: classes.delete,
       callback: () => {
-        if (car.id) this.handleDelete(car.id, () => fetch());
+        if (parameters.car.id) this.handleDelete(parameters.car.id);
       },
     });
+    this._selectBtn = new Button({
+      textContent: data.select,
+      className: classes.select,
+      callback: () => {
+        this.handleSelect(parameters.car);
+      },
+    });
+    this._startBtn = new Button({
+      textContent: data.start,
+      className: classes.start,
+      callback: () => this.handleStart(parameters.car.id),
+    });
+    this._stopBtn = new Button({
+      textContent: data.stop,
+      className: classes.stop,
+      callback: () => this.handleStop(parameters.car.id),
+    });
+    this._track = new BaseElement({ tag: 'div', className: classes.track });
+    this._model = new CarIcon({
+      className: 'svg',
+      color: parameters.car.color,
+    });
+    this._model.icon.classList.add(classes.car);
 
-    this.element.append(
-      this._id.element,
-      this._name.element,
-      this._model.icon,
-      this._picker.element,
-      this._updateBtn.element,
-      delButton.element
-    );
-  }
-
-  private async handleDelete(id: number, fetch: () => void) {
-    const isDeleted = await deleteCar(id);
-    if (isDeleted) {
-      fetch();
-    }
-  }
-
-  private async handleUpdate(id?: number) {
-    this._picker.element.disabled = true;
-    this._name.element.disabled = true;
-    this._updateBtn.element.disabled = true;
-
-    const updatedData = await updateCar({
-      id: id,
-      color: this._picker.element.value,
-      name: this._name.element.value,
+    const carControls = new BaseElement({
+      tag: 'div',
+      className: classes.controls,
     });
 
-    if (updatedData) {
-      this._picker.element.value = updatedData.color;
-      this._name.element.value = updatedData.name;
-    } else {
-      this._name.element.value = 'error';
-    }
+    this._carAnimator = new CarAnimator(this._model.icon);
 
-    this._picker.element.disabled = false;
-    this._name.element.disabled = false;
-    this._updateBtn.element.disabled = false;
+    carControls.append(
+      this._name.element,
+      this._selectBtn.element,
+      this._deleteBtn.element,
+      this._startBtn.element,
+      this._stopBtn.element
+    );
+
+    this._track.append(this._model.icon);
+
+    this.element.append(carControls.element, this._track.element);
   }
 
-  private handleChangeColor(color: string) {
-    let timeOut: undefined | number;
+  private handleSelect(car: ICar) {
+    this._tunning(car);
+  }
 
-    return () => {
-      if (timeOut) {
-        clearTimeout(timeOut);
-      }
+  private async handleStop(id?: number) {
+    if (id) {
+      await carEngineControl({
+        id,
+        status: 'stopped',
+      });
+    }
 
-      timeOut = setTimeout(() => {
-        this._model.color = color;
-      }, data.timeout);
-    };
+    this._carAnimator.reset();
+  }
+  private async handleDelete(id: number) {
+    const isDeleted = await deleteCar(id);
+    if (isDeleted) this._fetch();
+  }
+
+  public async handleStart(id?: number) {
+    if (id) {
+      const started: IStartResponse = await carEngineControl({
+        id,
+        status: 'started',
+      });
+      const { distance, velocity } = started;
+      const trackLength =
+        Number.parseFloat(
+          window
+            .getComputedStyle(this._track.element)
+            .width.slice(data.START_INDEX, data.END_INDEX)
+        ) -
+        Number.parseFloat(
+          window
+            .getComputedStyle(this._model.icon)
+            .width.slice(data.START_INDEX, data.END_INDEX)
+        );
+      this._carAnimator.start(distance, velocity, trackLength);
+      const drive = await carEngineControl({ id, status: 'drive' });
+
+      if (drive.error) this._carAnimator.stop();
+    }
   }
 
   get element() {
